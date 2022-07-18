6303101b73a364d369cf9c47c5700bf300879fc0
kos9310 <kos9310@gmail.com>
2022-07-18 15:31:24 +0900

    Video Upload Form

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -33,10 +33,13 @@ function RightMenu(props) {
   } else {
     return (
       <Menu mode={props.mode}>
-        <Menu.Item key="logout">
-          <a onClick={logoutHandler}>Logout</a>
+        <Menu.Item key="upload">
+          <a href='/video/upload'>Video</a>
         </Menu.Item>
-      </Menu>
+      <Menu.Item key="logout">
+        <a onClick={logoutHandler}>Logout</a>
+      </Menu.Item>
+    </Menu>
     )
   }
 }
