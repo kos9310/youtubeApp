41be814c7fa90e8debc5053eea64ae06d3189d80
kos9310 <kos9310@gmail.com>
2022-07-21 14:39:35 +0900

    SubscriptionPage

--- server/routes/video.js
+++ server/routes/video.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { Video } = require("../models/Video");
+const { Subscriber } = require('../models/Subscriber');
 
 const { auth } = require("../middleware/auth");
 const multer = require("multer");
 var ffmpeg = require("fluent-ffmpeg");
+
 ffmpeg.setFfmpegPath("C:\\Program Files\\ffmpeg\\bin\\ffmpeg.exe");
 // STORAGE MULTER CONFIG
 let storage = multer.diskStorage({
@@ -62,6 +64,32 @@ router.get('/getVideos', (req, res) => {
             if(err) return res.status(400).send(err);
             res.status(200).json({ success: true, videos})
         })
+})
+
+router.post('/getSubscriptionVideos', (req, res) => {
+
+    // 자신의 ID를 가지고 구독하는 사람들을 찾는다.
+    Subscriber.find({ userFrom: req.body.userFrom })
+        .exec(( err, subscriberInfo ) => {
+            if(err) return res.status(400).send(err);
+
+            let subscribedUser = [];
+
+            subscriberInfo.map((subscriber, i) => {
+                subscribedUser.push(subscriber.userTo);
+            })
+        
+
+        // 찾은 사람들의 비디오를 가지고 온다.
+        Video.find({ writer: { $in: subscribedUser }})
+            .populate('writer')
+            .exec((err, videos) => {
+                if(err) return res.status(400).send(err);
+                res.status(200).json({ success: true, videos })
+            })
+
+    })
+
 })
 
 router.post('/getVideoDetail', (req, res) => {
