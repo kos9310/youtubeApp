97a4d956aba1549fa49959d31eff4d67022b1195
kos9310 <kos9310@gmail.com>
2022-07-25 17:29:56 +0900

    ReplyComment

---
+++ client/src/components/views/VideoDetailPage/Sections/SingleComment.js
@@ -0,0 +1,75 @@
+import Axios from 'axios'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux';
+import { Comment, Avatar, Button, Input } from 'antd';
+
+const { TextArea } = Input;
+function SingleComment(props) {
+
+    const user = useSelector(state => state.user);
+
+    const [OpenReply, setOpenReply] = useState(false)
+    const [CommentValue, setCommentValue] = useState("")
+
+    const onClickReplyOpen = () => {
+        setOpenReply(!OpenReply)
+    }
+
+    const onHandleChange = (event) => {
+        setCommentValue(event.currentTarget.value)
+    }
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const variable = {
+            content: CommentValue,
+            writer: user.userData._id,
+            postId: props.postId,
+            responseTo: props.comment._id
+        }
+      
+        Axios.post('/api/comment/saveComment', variable)
+        .then(response => {
+            if(response.data.success) {
+            console.log(response.data.result)
+            setCommentValue("")
+            setOpenReply(false)
+            props.refreshFunction(response.data.result)
+            } else {
+            alert('코멘트를 저장하지 못 했습니다.')
+            }
+        })
+    }
+
+    const actions = [
+        <span onClick={onClickReplyOpen} key="comment-basic-reply-to"> Reply to</span>
+    ]
+  return (
+    <div>
+        {props.comment.writer && (
+        <Comment
+            actions={actions}
+            author={props.comment.writer.name}
+            avatar={<Avatar src={props.comment.writer.image} alt />}
+            content={ <p> {props.comment.content} </p>}
+        />
+        )}
+        {OpenReply && 
+            <form style={{ display: 'flex' }} omSubmit={onSubmit} >
+            <textarea
+            style={{ width: '100%', borderRadius: '5px' }}
+            onChange={onHandleChange}
+            value={CommentValue}
+            placeholder='코멘트를 작성해 주세요'
+            />
+            <br />
+            <button style={{ width: '20%', height: '52px' }} onClick={onSubmit} >Submit</button>
+            </form>
+        }
+    </div>
+    
+  )
+}
+
+export default SingleComment
--- client/src/components/views/VideoDetailPage/VideoDetailPage.js
+++ client/src/components/views/VideoDetailPage/VideoDetailPage.js
@@ -3,6 +3,7 @@ import { Row, Col, List, Avatar } from 'antd'
 import Axios from 'axios';
 import SideVideo from './Sections/SideVideo';
 import Subscribe from './Sections/Subscribe';
+import Comment from './Sections/Comment';
 
 function VideoDetailPage(props) {
 
@@ -10,6 +11,8 @@ function VideoDetailPage(props) {
     const variable = { videoId: videoId}
 
     const [VideoDetail, setVideoDetail] = useState([])
+
+    const [Comments, setComments] = useState([])
 
     useEffect(() => {
       
@@ -23,9 +26,26 @@ function VideoDetailPage(props) {
                 }
             })
 
+        Axios.post('/api/comment/getComments', variable)
+            .then(response => {
+                if(response.data.success) {
+                    console.log(response.data);
+                    setComments(response.data.comments)
+                } else {
+                    alert('코멘트 정보를 가져오는데 실패했습니다.')
+                }
+            })
+
     }, [])
     
+    const refreshFunction = (newComment) => {
+        setComments(Comments.concat(newComment))
+    }
+
     if(VideoDetail.writer) {
+
+        const subscribeButton = VideoDetail.writer._id !== localStorage.getItem('userId') && <Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>
+        
         return (
             <Row gutter={[16, 16]}>
                 <Col lg={18} xs={24} >
@@ -34,7 +54,7 @@ function VideoDetailPage(props) {
                     <video style={{ width: '100%'}} src={`http://localhost:5000/${VideoDetail.filePath}`} controls />
         
                     <List.Item
-                        actions={[<Subscribe userTo={VideoDetail.writer._id} userFrom={localStorage.getItem('userId')}/>]}
+                        actions={[ subscribeButton ]}
                     >
                         <List.Item.Meta
                             avatar={<Avatar src={VideoDetail.writer.image} />}
@@ -44,6 +64,7 @@ function VideoDetailPage(props) {
                     </List.Item>
         
                     {/* Comments */}
+                    <Comment refreshFunction={refreshFunction} commentLists={Comments} postId={videoId} />
         
                 </div>
                 </Col>
